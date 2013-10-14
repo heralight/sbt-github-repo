73a2e82bd2e7b3a7ef34983e48460a08772f494a
suikwasha <suikwasha@gmail.com>
2013-10-14 22:55:01 +0900

    removed plugin.scala

--- src/main/scala/com/github/suikwasha/GithubRepoPlugin.scala
+++ src/main/scala/com/github/suikwasha/GithubRepoPlugin.scala
@@ -3,19 +3,17 @@ package com.github.suikwasha
 import sbt._
 import sbt.Keys._
 
-import com.typesafe.sbt.git.GitRunner
 import org.eclipse.jgit.api.{ListBranchCommand, Git}
 import scala.collection.JavaConversions
 import org.eclipse.jgit.transport.RefSpec
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.pgm.Main
 
 trait GithubRepoKeys {
-  val localRepo = SettingKey[File]("local-repo")
-  val githubRepo = SettingKey[String]("github-repo")
-  val createRepo = TaskKey[Unit]("create-repo")
-  val publishToGithubRepo = TaskKey[Unit]("publish-to-github-repo")
+  val githubRepo = SettingKey[String]("gitHubRepo")
 }
 
-case class GitWrapper(val jgit: Git, val repo: File, val remote: String, val taskStreams: TaskStreams)
+case class GitWrapper(val jgit: Git, val remote: String, val logger: Logger)
 
 object GithubRepoPlugin extends Plugin with GithubRepoKeys {
 
@@ -38,11 +36,12 @@ object GithubRepoPlugin extends Plugin with GithubRepoKeys {
     val jgit = gw.jgit
     val branches: ListBranchCommand = jgit.branchList
     val refs = JavaConversions.asScalaIterator(branches.call().iterator())
-    if(refs.find(_.getName.equals(branchName)).isEmpty) {
+    if(refs.find(_.getName.endsWith(branchName)).isEmpty) {
       val branchCreateCommand = jgit.branchCreate
       branchCreateCommand.setName(branchName)
       val ref = branchCreateCommand.call()
       ref.getName.equals(branchName)
+      gw.logger.info("branch " + branchName + " created")
     } else {
       false
     }
@@ -54,6 +53,7 @@ object GithubRepoPlugin extends Plugin with GithubRepoKeys {
       val jgit = gw.jgit
       val config = jgit.getRepository.getConfig
       config.setString("remote", remote, "url", url)
+      gw.logger.info("added remote repository " + remote + " " + url)
       true
     } else {
       false
@@ -65,6 +65,7 @@ object GithubRepoPlugin extends Plugin with GithubRepoKeys {
     val checkoutCommand = jgit.checkout
     checkoutCommand.setName(branchName)
     checkoutCommand.call
+    gw.logger.info("checked out branch " + branchName)
   }
 
   def push(remote: String, branch: String)(implicit gw: GitWrapper) = {
@@ -73,5 +74,54 @@ object GithubRepoPlugin extends Plugin with GithubRepoKeys {
     pushCommand.setRemote(remote)
     pushCommand.setRefSpecs(new RefSpec(branch + ":" + branch))
     pushCommand.call
+    gw.logger.info("pushed to " + remote + ":" + branch)
   }
+
+  def add()(implicit gw: GitWrapper) = {
+    val jgit = gw.jgit
+    jgit.add.addFilepattern(".").call()
+    gw.logger.info("added files to commit.")
+  }
+
+  def commit(message: String)(implicit gw: GitWrapper) = {
+    val jgit = gw.jgit
+    jgit.commit().setMessage(message).call()
+    gw.logger.info("commit created.")
+  }
+
+  private def pushToGithub(file: File, remote: String, logger: Logger = ConsoleLogger()) = {
+    val jgit = createRepositoryTask(file)
+    implicit val gw = new GitWrapper(jgit, remote, logger)
+    add()
+    // TODO: use version number and build date for commit message
+    commit("commit")
+    createBranch("gh-pages")
+    checkout("gh-pages")
+    addRemoteRepository(Origin, remote)
+    push(Origin, "gh-pages")
+
+    // TODO: use jgit output
+    gw.logger.success("pushed to gh-pages on " + remote + " successfully.")
+  }
+
+  def publishTargetDir(state: State, args: Seq[String]) = {
+    val extracted = Project.extract(state)
+    val repoDirOpt: Option[File] = extracted.getOpt(target).map(_ / "repo")
+    repoDirOpt match {
+      case Some(file) => {
+        val publishToSetting = Seq(publishTo := repoDirOpt.map(t => Resolver.file("file", t)))
+        val newState = extracted.append(publishToSetting, state)
+        Command.process("publish", newState)
+
+        args.headOption match {
+          case Some(remote) => pushToGithub(file, remote)
+          case None => throw new IllegalStateException()
+        }
+      }
+      case None => throw new IllegalStateException()
+    }
+    state
+  }
+
+  override val settings = Seq(commands += Command.args("publish-ghrepo", "<github_url>")(publishTargetDir))
 }
